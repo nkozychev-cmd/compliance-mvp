8bab1201884c4e72b1a46d49f4ca3a6adeb2964e
nkozychev-cmd <nkozychev@gmail.com>
2025-10-15 22:37:03 +0200

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,11 @@
-import { supabase } from '../supabaseClient';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// The root of your React app
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)

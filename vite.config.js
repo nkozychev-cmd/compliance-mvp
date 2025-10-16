f56f2f0e8705f58da3d05d987b8a584f90bbc5c7
nkozychev-cmd <nkozychev@gmail.com>
2025-10-16 22:33:14 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Automatically switch base for local vs. GitHub Pages
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/compliance-mvp/' : '/',
   plugins: [react()],
+  base: './', // 👈 important line
 })

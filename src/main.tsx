f56dc4cb9be3c91d05259f51a682d438840f8d5a
nkozychev-cmd <nkozychev@gmail.com>
2025-10-16 23:00:17 +0200

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// The root of your React app
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Root element not found')
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
